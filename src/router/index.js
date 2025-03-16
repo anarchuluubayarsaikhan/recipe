1e1e290b2452b070ffed342ec02ff882122d0212
anarchuluubayarsaikhan <anarchulu@gmail.com>
2025-03-16 20:22:22 +0800

    preparing for deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,32 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-
-import Home from '@/views/Home.vue';
-import CreateRecipe from '@/views/CreateRecipe.vue';
-import MyRecipes from '@/views/MyRecipes.vue';
-import RecipeDetails from '@/views/RecipeDetails.vue';
-import SavedRecipes from '@/views/SavedRecipes.vue';
-import LoginPage from '@/views/LoginPage.vue';
-
+import Home from "@/views/Home.vue";
+import CreateRecipe from "@/views/CreateRecipe.vue";
+import MyRecipes from "@/views/MyRecipes.vue";
+import RecipeDetails from "@/views/RecipeDetails.vue";
+import SavedRecipes from "@/views/SavedRecipes.vue";
+import LoginPage from "@/views/LoginPage.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '/', component: LoginPage },  
-  { path: '/home', component: Home },  
-  { path: '/newrecipe', component: CreateRecipe },  
-  { path: '/userrecipe', component: MyRecipes },  
-  { path: '/saved', component: SavedRecipes },  
+  { path: "/", component: LoginPage },
+  { path: "/home", component: Home },
+  { path: "/newrecipe", component: CreateRecipe },
+  { path: "/userrecipe", component: MyRecipes },
+  { path: "/saved", component: SavedRecipes },
   {
-    path: '/details/:id',
-    name: 'details',
+    path: "/details/:id",
+    name: "details",
     component: RecipeDetails,
-    props: true, 
+    props: true,
   },
 ];
 
 const router = new VueRouter({
   routes,
-  mode: 'history',
+  mode: "history",
 });
 
 export default router;
